7cc030a8216aa39eb4e289038ffb1f6c91bce9c0
Hunter Thomas Jamison <hjamisonwork@gmail.com>
2024-04-03 12:07:49 -0700

    Adding talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Ithaca, NY",
+    42.44726,
+    -76.47862
   ],
   [
-    "London, UK",
+    "San Francisco, Ca",
     51.5073219,
     -0.1276473
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Paris, France",
+    48.84477,
+    2.35654
   ]
 ];
